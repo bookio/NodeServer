62d76504e7fa60a3cf1c421526e5ec779681a911
Magnus Egelberg <magnus@egelberg.se>
2015-01-21 11:41:15 +0100

    -

--- app.js
+++ app.js
@@ -2,12 +2,40 @@ var express = require('express');
 var app = express();
 var url = 'postgres://kvfqksuntzehlc:dwO7w6TE7naBA56bMf55dgNTPV@ec2-54-204-47-70.compute-1.amazonaws.com:5432/d3uoq4q0g7rpeb?ssl=true';
 var pg = require('pg');
+var sprintf = require('./sprintf.js');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 
+function Rental() {
 
+	var self = this;
+
+	self.
+	self.id = 0;
+	
+	self.save = function() {
+		
+	}		
+}
+
+
+
+function getClientID(client, request) {
+	
+
+	client.query('SELECT client_id FROM users WHERE id=(SELECT user_id FROM sessions WHERE sid=$1)', [request.headers.authorization], function(error, result) {
+
+
+		if (error)
+			throw error; 
+			
+		console.log(result.rows);
+			
+	});
+
+}
 app.get('/db', function (request, response) {
 	
 	console.log('Connecting...');
@@ -36,45 +64,36 @@ app.get('/db', function (request, response) {
 
 app.get('/rentals/:id', function (request, response) {
 	
-	console.log('Connecting...');
+	console.log('Request:', request.headers.authorization);
+//	console.log('Response:', response);
 	
-	pg.connect(url, function(err, client, done) {
-		if (err) {
-			console.error(err);
-		}
-		else {
+	try {
+		pg.connect(url, function(error, client, done) {
+			if (error)
+				throw error;
 			
-			client.query('SELECT * FROM rentals where client_id=1 and id=' + request.params.id, function(err, result) {
+			getClientID(client, request);
+
+			client.query('SELECT * FROM rentals WHERE client_id=$1 AND id=$2', [request.params.id, 1], function(error, result) {
 				done();
 
-				if (err) { 
-					console.error(err); response.send("Error " + err); 
-				}
-				else {
-					response.send(JSON.stringify(result.rows));
-				}
+				if (error)
+					throw error; 
+					
+				response.send(JSON.stringify(result.rows, null, 4));
 			});
-			
-		}
-	});
-});
-
-app.get('/', function(request, response) {
-	console.log('ConnectingX...');
-	
-	pg.connect(url, function(err, client, done) {
-		client.query('SELECT * FROM clients', function(err, result) {
-			done();
-			if (err) { 
-				console.error(err); response.send("Error " + err); 
-			}
-			else {
-				response.send(result.rows);
-			}
+				
 		});
-	});
-
+		
+	}
+	catch (error) {
+		console.error(error);
+		
+	}
+	
 });
+
+
 
 app.listen(app.get('port'), function() {
 	console.log("Node app is running at localhost:" + app.get('port'));
