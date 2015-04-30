58a37fae7e01a88784c8bfea728f41ef123ad42d
Magnus Egelberg <magnus@egelberg.se>
2015-04-30 11:21:29 +0200

    -

--- sequelize.js
+++ sequelize.js
@@ -3,6 +3,11 @@ var Sequelize = require('sequelize');
 //var url = 'postgres://kvfqksuntzehlc:dwO7w6TE7naBA56bMf55dgNTPV@ec2-54-204-47-70.compute-1.amazonaws.com:5432/d3uoq4q0g7rpeb?ssl=true&sslmode=require';
 
 
+// Make UTC dates behave...
+require('pg').types.setTypeParser(1114, function(stringValue) {
+	return new Date(stringValue + "+0000");
+});
+
 var sequelize = new Sequelize('dakh54pfbr5h44', 'tngvlsuocnzuzw', 'mShWT2fKPbmoXsG7cDE0PH21Jl', {
 	host: 'ec2-50-16-190-77.compute-1.amazonaws.com',
 	dialect: 'postgres',
